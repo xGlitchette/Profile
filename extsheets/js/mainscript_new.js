d57433189b62d11ef4b8fb2675ea0ecf6f09ff85
Dominique Vink <47154206+xGlitchette@users.noreply.github.com>
2022-04-07 09:18:44 -0400

    things
    things !

--- extsheets/js/mainscript_new.js
+++ extsheets/js/mainscript_new.js
@@ -1,11 +1,31 @@
 function openBox(overlay) {
     document.getElementById('home').style.display = "none";
+    document.getElementById('closebtn_' + overlay).style.display = "block";
     document.getElementById(overlay).style.display = "block";
     document.getElementById('box').style.height = "640px";
 }
 
 function closeBox(overlay) {
     document.getElementById('home').style.display = "block";
+    document.getElementById('closebtn_' + overlay).style.display = "none";
     document.getElementById(overlay).style.display = "none";
     document.getElementById('box').style.height = "320px";
+}
+
+function age(){
+    var dob = new Date("12/19/2001");  
+    //calculate month difference from current date in time  
+    var month_diff = Date.now() - dob.getTime();  
+      
+    //convert the calculated difference in date format  
+    var age_dt = new Date(month_diff);   
+      
+    //extract year from date      
+    var year = age_dt.getUTCFullYear();  
+      
+    //now calculate the age of the user  
+    var age = Math.abs(year - 1970);  
+      
+    //display the calculated age  
+    document.getElementById('dob').innerHTML = "12/19/2001 (" + age + ")";  
 }
